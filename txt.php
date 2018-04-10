3f49fb8cc68873373301241018810539a44e24c3
Allen <kevertje7@hotmail.com>
2018-04-10 23:51:14 +0200

    php nightmode
    mode a session variable to regulate nightmode css stylesheets

--- txt.php
+++ txt.php
@@ -1,8 +1,27 @@
+<?php
+	// Start Session
+	session_start();
+	
+	// Set nightmode css for the first time
+	if(!isset($_SESSION['nightmode'])){
+		$_SESSION['bookmarks'] = false;
+	}
+	
+	// Set css-file based on nightmode
+	if(isset($_SESSION['nightmode']) && $_SESSION['nightmode'] == true){
+		$nightmode = "night.css";
+	} else if(isset($_SESSION['nightmode']) && $_SESSION['nightmode'] == false) {
+		$nightmode = "day.css";
+	}
+?>
 
 <!DOCTYPE html>
 <html>
 <head lang="en" >
-    <title>Unicorn pasta</title>
+	
+	<link rel="stylesheet" type="css/<?php echo nightmode ?>" href="theme.css">
+    
+	<title>Unicorn pasta</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 	
