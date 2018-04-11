5a3db18656a7f2c982228095b5b1f231292f3ce5
Flyingdroplet <edward.debruyne1997@hotmail.com>
2018-04-11 19:09:12 +0200

    test night
    button

--- Php/edward.php
+++ Php/edward.php
@@ -5,15 +5,37 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-	<link rel="stylesheet" href="edward.css.css">
+	<link rel="stylesheet" href="edward.css">
     <title>Night / Day Mode</title>
   
 </head>
-<body id="body">
-
-<div class="form">
-<input type="checkbox" id="bubble" />
-<label class="bubble" for="bubble"></label>
-</div></body>
-
+<body id="body" class="c-window">
+<div class="the-container">
+  
+  <input type="checkbox" id="toggle" />
+  <label for="toggle"></label>
+  
+  <div class="day-night-cont">
+      <span class="the-sun"></span>
+      <div class="the-moon"><span class="moon-inside"></span></div>
+   </div>
+  
+  <div class="switch">
+    <div class="button">
+      <div class="b-inside"></div>
+    </div>
+  </div>
+  
+  <!-- This is an HTML Comment
+<div class="c-window">
+    <span class="the-sun"></span>
+    <span class="the-moon"></span>
+    
+   -->
+    
+  </div>
+  
+</div>
+  
+</div>
 </html>
