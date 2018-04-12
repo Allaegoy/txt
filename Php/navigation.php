c638585084e777670336f671737d456ef21c750b
Flyingdroplet <edward.debruyne1997@hotmail.com>
2018-04-12 21:35:01 +0200

    slider is back

--- Php/navigation.php
+++ Php/navigation.php
@@ -22,4 +22,33 @@
 			<a href="Php/stylemode.php?mode=rainbow">Rainbow-mode</a>
 		</div>
 	</div>
-</div>
+
+	<div class="slidecontainer">
+		
+		<input type="range" min="0" max="2" value="0" class="slider" id="myRange" name="nightslider">
+		</form> 
+		<p>Value: <span id="demo"></span></p>
+	</div>
+<script>
+	var slider = document.getElementById("myRange");
+	var output = document.getElementById("demo");
+
+	slider.value = 0;
+
+	function showNightmodeSliderText() {
+	if (slider.value == 0){
+		output.innerHTML = "day";
+	} else {
+		if(slider.value == 1) {
+			output.innerHTML = "night";
+		}else{
+			output.innerHTML = "rainbow mode";
+		}
+	}
+	}
+
+	slider.oninput = showNightmodeSliderText;
+	showNightmodeSliderText();
+</script>
+	</div>
+	
