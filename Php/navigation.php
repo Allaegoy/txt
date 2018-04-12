7a80395b4693d6e758b15f76f5096be25f1ae8e9
Allen <kevertje7@hotmail.com>
2018-04-12 21:20:02 +0200

    Revert "Sandbox set to original"
    This reverts commit e6e4eecd64ca48c0c8b7d231dbd021b15a9dc938.

--- Php/navigation.php
+++ Php/navigation.php
@@ -18,10 +18,47 @@
 		<i class="fa fa-caret-down"></i>
 		</button>
 		<div class="dropdown-content">
+<<<<<<< HEAD
 			<a href="Php/stylemode.php/?mode=night">Night-mode</a>
 			<a href="Php/stylemode.php/?mode=day">Day-mode</a>
 			<a href="Php/stylemode.php/?mode=rainbow">Rainbow-mode</a>
 		</div>
 	</div>
+=======
+			<a href="#">Night-mode</a>
+			<a href="#">Day-mode</a>
+			<a href="#">Rainbow-mode</a>
+		</div>
+	</div>
+	<div class="slidecontainer">
+		<input type="range" min="0" max="1" value="0" class="slider" id="myRange" name="nightslider">
+		</form> 
+		<p>Value: <span id="demo"></span></p>
+	</div>
+	
+	<script>
+
+
+	var slider = document.getElementById("myRange");
+	var output = document.getElementById("demo");
+
+	slider.value = 0;
+
+	function showNightmodeSliderText() {
+	if (slider.value == 0){
+		output.innerHTML = "day";
+	} else {
+		if(slider.value == 1) {
+			output.innerHTML = "night";
+		}else{
+			output.innerHTML = "error: nightmode not found";
+		}
+	}
+	}
+
+	slider.oninput = showNightmodeSliderText;
+	showNightmodeSliderText();
+
+>>>>>>> parent of e6e4eec... Sandbox set to original
 	</script>
 </div>
