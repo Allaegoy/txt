d82548852c1d3e68fc0f153d04d86c503aa30b62
Flyingdroplet <32341446+Flyingdroplet@users.noreply.github.com>
2018-04-18 14:19:27 +0200

    fucking shit ding

--- Php/edward.php
+++ Php/edward.php
@@ -5,16 +5,23 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-	<link rel="stylesheet" href="edward.css">
+	<link id="pagestyle" rel="stylesheet" type="text/css" href="edward.css">
     <title>Night / Day Mode</title>
   
 </head>
 <body id="body" class="c-window">
 <div class="the-container">
   
-  <input type="checkbox" id="toggle" />
+  <input type="checkbox" id="toggle"  onclick="myFunction()" />
   <label for="toggle"></label>
-  
+
+  <script>
+function myFunction() {
+    alert("The ontoggle event occured");
+	
+	document.getElementById('pagestyle').setAttriubute('href',sheet);
+}
+</script>
   <div class="day-night-cont">
       <span class="the-sun"></span>
       <div class="the-moon"><span class="moon-inside"></span></div>
@@ -56,5 +63,19 @@ function off() {
     document.getElementById("overlay").style.display = "none";
 }
 </script>
+
+
+
+
+
+
+
+
+
+<link rel="stylesheet" type="text/css" title="edward" href="edward.css">
+<link rel="alternate stylesheet" type="text/css" title="dark" href="dark.css">
+<input type="submit" onclick="switch_style('edward');return false;" name="theme" value="edward Theme" id="edward">
+// <input type="submit" onclick="switch_style('dark');return false;" name="theme" value="dark Theme" id="dark">
+
      </body>
 </html>
