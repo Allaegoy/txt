8b116ec8596dd2f7d6347b8b2401a4a262b30df2
Allen <kevertje7@hotmail.com>
2018-04-18 15:18:09 +0200

    merge conflict resolved
    see title

--- Php/edward.php
+++ Php/edward.php
@@ -43,11 +43,9 @@
 	<div class="c-window">
     <span class="the-sun"></span>
     <span class="the-moon"></span>
-    
-   -->
-    
-<<<<<<< HEAD
 	</div>
+   -->
+
 	<div id="overlay" onclick="off()">
 		<div id="text">Overlay Text</div>
 	</div>
@@ -70,49 +68,6 @@
 	<link rel="alternate stylesheet" type="text/css" title="dark" href="dark.css">
 	<input type="submit" onclick="switch_style('edward.css');" name="theme" value="edward Theme" id="edward">
 	// <input type="submit" onclick="switch_style('dark.css');" name="theme" value="dark Theme" id="dark">
-=======
-  </div>
-  
-
-  
-
-<div id="overlay" onclick="off()">
-  <div id="text">Overlay Text</div>
-</div>
-
-<div style="padding:20px">
-  <h2>Overlay with Text</h2>
-  <button onclick="on()">Turn on overlay effect</button>
-</div>
-
-<script>
-function on() {
-    document.getElementById("overlay").style.display = "block";
-	swapstylesheet('dark.css');
-}
-
-function off() {
-    document.getElementById("overlay").style.display = "none";
-	swapstylesheet('dark.css');
-}
-</script>
-
-
-
-
-
-
-
-
-
-<link rel="stylesheet" type="text/css" title="edward" href="edward.css">
-<link rel="alternate stylesheet" type="text/css" title="dark" href="dark.css">
-<input type="submit" onclick="switch_style('edward.css');return false;" name="theme" value="edward Theme" id="edward">
-// <input type="submit" onclick="switch_style('dark.css');return false;" name="theme" value="dark Theme" id="dark">
-<<<<<<< HEAD
->>>>>>> 02ac0eed87b7edc46eb20d68e85e2712231c3cb9
-=======
->>>>>>> 02ac0eed87b7edc46eb20d68e85e2712231c3cb9
 
 	</body>
 </html>
