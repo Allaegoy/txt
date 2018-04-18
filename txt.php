bb1ce5c120129c1e5ee4d0a712681e1b5e8d20d7
Flyingdroplet <32341446+Flyingdroplet@users.noreply.github.com>
2018-04-18 15:33:08 +0200

    id pagestyle

--- txt.php
+++ txt.php
@@ -22,7 +22,7 @@ session_start();
 	
     <link rel="stylesheet" href="css/txt.css">
 	<link rel="stylesheet" href="css/slider.css">
-	<link rel="stylesheet" href="css/<?php echo $mode; ?>/navbar.css">
+	<link id="pagestyle" rel="stylesheet"  href="css/<?php echo $mode; ?>/navbar.css">
 	<link rel="stylesheet" href="css/<?php echo $mode; ?>/mode.css">
 	
 	
